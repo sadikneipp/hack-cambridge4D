5f694e051bcd3c96d0b24b03b57193cf0d3923ee
sadikneipp <sadi.neto13@gmail.com>
2019-01-20 07:10:30 +0000

    rpi initial approach

--- BackendAlexa.py
+++ BackendAlexa.py
@@ -1,13 +1,13 @@
 import acquisition
 from flask import Flask, jsonify, request
-import logic
+# import logic
 from PIL import Image
 from io import BytesIO
 import base64
 import pygame
 import os
 
-model = logic.model_load()
+# model = logic.model_load()
 
 app = Flask(__name__)
 medications = ['ibuprofen', 'paracetamol']
@@ -18,21 +18,22 @@ def alexareq():
     img = acquisition.camera.snap_ss(webcam)
     pil_string_image = pygame.image.tostring(img,"RGB",False)
     im = Image.frombytes("RGB",(640, 480),pil_string_image)
-
     buffered = BytesIO()
     im.save(buffered, format="JPEG")
     b64_im = base64.b64encode(buffered.getvalue())
-    prediction = logic.predict(model, b64_im)
-    print(prediction)
-    acquisition.camera.stop_camera(webcam)
+    return jsonify({'image' : b64_im})
 
-    #for demo
-    if len(os.listdir('demo')) > 0:
-        os.remove('demo/' + os.listdir('demo')[0])
-    im.save('demo/' + prediction + '.jpg')
+    # prediction = logic.predict(model, b64_im)
+    # print(prediction)
+    # acquisition.camera.stop_camera(webcam)
+
+    # #for demo
+    # if len(os.listdir('demo')) > 0:
+    #     os.remove('demo/' + os.listdir('demo')[0])
+    # im.save('demo/' + prediction + '.jpg')
     
-    return jsonify({'alexaReq': str(int(prediction in medications)),
-                    'medication': str(prediction).replace('_', ' ')})
+    # return jsonify({'alexaReq': str(int(prediction in medications)),
+    #                 'medication': str(prediction).replace('_', ' ')})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
